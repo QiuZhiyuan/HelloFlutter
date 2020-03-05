41c866c3cd63999cb8caa463e834a61f987b71bd
qiuzhiyuan <845026103@qq.com>
2020-03-05 16:37:31 +0800

    print touch position

--- lib/event/touch_event.dart
+++ lib/event/touch_event.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -5,6 +7,8 @@ class EventWidgets {
   static String sContent = '下面是个待点击的按钮';
 
   Function callback;
+
+  static int count = 0;
 
   EventWidgets(Function callback) {
     this.callback = callback;
@@ -22,11 +26,14 @@ class EventWidgets {
 
   Widget getButtonView() {
     return MaterialButton(
-      onPressed: onClick,
+      onPressed: _onClick,
       child: Container(
         width: 100,
         height: 30,
         color: Color(0xffff00ff),
+        child: Center(
+          child: Text('这是单击按钮'),
+        ),
       ),
     );
   }
@@ -34,7 +41,8 @@ class EventWidgets {
   // InkResponse是Button的基类
   Widget getInkResponse() {
     return InkResponse(
-      onDoubleTap: onDoubleTap,
+      onDoubleTap: _onDoubleTap,
+      onTapDown: _onTapDown,
       child: Container(
         width: 100,
         height: 30,
@@ -46,13 +54,24 @@ class EventWidgets {
     );
   }
 
-  onClick() {
+  _onClick() {
     sContent = '已点击按钮';
     callback();
   }
 
-  onDoubleTap() {
+  _onDoubleTap() {
     sContent = '已经被双击';
     callback();
+  }
+
+  _onTapDown(TapDownDetails details) {
+    log('qiuzhiyuan: dx:' +
+        details.globalPosition.dx.toString() +
+        " dy" +
+        details.globalPosition.dy.toString());
+    log('qiuzhiyuan dx:' +
+        details.localPosition.dx.toString() +
+        ' dy:' +
+        details.localPosition.dy.toString());
   }
 }
